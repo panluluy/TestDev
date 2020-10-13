5c688055c2f3bae4748ef84aa0c17ed355df6ffe
panluluy <pan_luluy@163.com>
2020-10-13 20:45:44 +0800

    'test_playform'

---
+++ test_playform/app_manage/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    """项目表"""
+
+    name = models.CharField('名称',max_length=100,blank=False,default='')
+    describe = models.TextField('描述',default='')
+    status = models.BooleanField('状态',default=True)
+    update_time = models.DateTimeField('更新时间',auto_now=True)
+    create_time = models.DateTimeField('创建时间',auto_now_add=True)
