b9e3cf7c06da26b2911582fe058e5883aa182856
panluluy <pan_luluy@163.com>
2020-10-15 21:23:14 +0800

    添加新内容

--- test_playform/app_manage/views.py
+++ test_playform/app_manage/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from app_manage.models import Project
 
 # Create your views here.
 @login_required
 def manage(request):
-    return render(request,'manager.html')
+    project_list = Project.objects.all()
+    return render(request,'manager.html',{'projects':project_list})
